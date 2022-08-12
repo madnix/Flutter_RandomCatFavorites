7df743cd4bbe461346fbbd4ba4a28ff46a9e34ea
madnix <madnix2033@gmail.com>
2022-08-12 17:38:26 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,193 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  // main() 함수에서 async를 쓰려면 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // shared_preferences 인스턴스 생성 (SharedPreferences = 좋아요 정보를 키,밸류 형식으로 내장 메모리에 저장)
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CatService(prefs)),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+/// 고양이 서비스
+class CatService extends ChangeNotifier {
+  // 고양이 사진 담을 변수
+  List<String> catImages = [];
+
+  // 좋아요 사진
+  List<String> favoriteImages = [];
+
+  // SharedPreferences 인스턴스
+  SharedPreferences prefs;
+
+  CatService(this.prefs) {
+    getRandomCatImages();
+
+    // favorites로 저장된 favoriteImages를 가져옵니다.
+    // 저장된 값이 없는 경우 null을 반환하므로 이때는 빈 배열을 넣어줍니다.
+    favoriteImages = prefs.getStringList("favorites") ?? [];
+  }
+
+  // 랜덤 고양이 사진 API 호출
+  void getRandomCatImages() async {
+    Response resuld = await Dio().get(
+        "https://api.thecatapi.com/v1/images/search?limit=10&mime_types=jpg");
+
+    for (int i = 0; i < resuld.data.length; i++) {
+      var map = resuld.data[i];
+      catImages.add(map['url']);
+    }
+    notifyListeners(); // 컨슈머 아래 캣서비스 전체 새로고침
+  }
+
+  void toggleFavoriteImage(String catImage) {
+    if (favoriteImages.contains(catImage)) {
+      favoriteImages.remove(catImage);
+    } else {
+      favoriteImages.add(catImage);
+    }
+
+    // favoriteImages를 favorites라는 이름으로 저장하기
+    prefs.setStringList("favorites", favoriteImages);
+
+    notifyListeners(); // 컨슈머 아래 캣서비스 전체 새로고침
+  }
+}
+
+/// 홈 페이지
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CatService>(
+      builder: (context, catService, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("랜덤 고양이"),
+            backgroundColor: Colors.amber,
+            actions: [
+              // 좋아요 페이지로 이동
+              IconButton(
+                icon: Icon(Icons.favorite),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FavoritePage()),
+                  );
+                },
+              )
+            ],
+          ),
+          // 고양이 사진 목록
+          body: GridView.count(
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 8,
+            padding: EdgeInsets.all(8),
+            crossAxisCount: 2,
+            children: List.generate(
+              catService.catImages.length,
+              (index) {
+                String catImage = catService.catImages[index];
+                return GestureDetector(
+                    onTap: () {
+                      catService.toggleFavoriteImage(catImage);
+                    },
+                    child: Stack(
+                      children: [
+                        Positioned.fill(
+                            child: Image.network(catImage, fit: BoxFit.cover)),
+                        Positioned(
+                          right: 8,
+                          bottom: 8,
+                          child: Icon(
+                            Icons.favorite,
+                            color: catService.favoriteImages.contains(catImage)
+                                ? Colors.amber
+                                : Colors.transparent,
+                          ),
+                        )
+                      ],
+                    ));
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+/// 좋아요 페이지
+class FavoritePage extends StatelessWidget {
+  const FavoritePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CatService>(
+      builder: (context, catService, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("좋아요"),
+            backgroundColor: Colors.amber,
+          ),
+          body: GridView.count(
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 8,
+            padding: EdgeInsets.all(8),
+            crossAxisCount: 2,
+            children: List.generate(
+              catService.favoriteImages.length,
+              (index) {
+                String catImage = catService.favoriteImages[index];
+                return GestureDetector(
+                    onTap: () {
+                      catService.toggleFavoriteImage(catImage);
+                    },
+                    child: Stack(
+                      children: [
+                        Positioned.fill(
+                            child: Image.network(catImage, fit: BoxFit.cover)),
+                        Positioned(
+                          right: 8,
+                          bottom: 8,
+                          child: Icon(
+                            Icons.favorite,
+                            color: catService.favoriteImages.contains(catImage)
+                                ? Colors.amber
+                                : Colors.transparent,
+                          ),
+                        )
+                      ],
+                    ));
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
